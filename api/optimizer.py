2ff06780fa6b730ea5ce677cc56c1781bb8b433e
robe <robert.domingues.97616@a.fecaf.com.br>
2025-08-30 11:40:21 -0300

    Update optimizer.py

--- api/optimizer.py
+++ api/optimizer.py
@@ -1,27 +1,21 @@
 import networkx as nx
 import osmnx as ox
 
-ox.settings.log_console = False
-ox.settings.use_cache = True
+def optimize_route(points):
+    """
+    Usa A* em grafo OSMnx para encontrar caminho mais curto
+    """
+    if len(points) < 2:
+        return {"error": "Precisa de pelo menos 2 pontos"}
 
-def optimize_routes(points):
-    """Calcula a melhor rota usando A*"""
-    center = (points[0]["lat"], points[0]["lon"])
-    G = ox.graph_from_point(center, dist=5000, network_type="drive")
-
+    G = ox.graph_from_point((points[0]["lat"], points[0]["lon"]), dist=3000, network_type="drive")
+    
     route = []
-    total_dist = 0
-    total_time = 0
-
-    for i in range(len(points) - 1):
+    for i in range(len(points)-1):
         orig = ox.distance.nearest_nodes(G, points[i]["lon"], points[i]["lat"])
         dest = ox.distance.nearest_nodes(G, points[i+1]["lon"], points[i+1]["lat"])
         path = nx.astar_path(G, orig, dest, weight="length")
-        dist = sum(ox.utils_graph.get_route_edge_attributes(G, path, "length"))
-        time = dist / (40 * 1000 / 60)  # 40km/h
-
-        route.extend(path)
-        total_dist += dist
-        total_time += time
-
-    return route, round(total_dist/1000, 2), round(total_time, 2)
+        coords = [(G.nodes[n]["y"], G.nodes[n]["x"]) for n in path]
+        route.extend(coords)
+    
+    return {"route": route}
