61418f515ca26ccef3b3f83db6ec37599127e416
robe <robert.domingues.97616@a.fecaf.com.br>
2025-08-30 11:40:48 -0300

    Update ml_model.py

--- api/ml_model.py
+++ api/ml_model.py
@@ -1,1 +1,15 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
+class DeliveryTimePredictor:
+    def __init__(self, model_type="linear"):
+        if model_type == "linear":
+            self.model = LinearRegression()
+        else:
+            self.model = RandomForestRegressor(n_estimators=100, random_state=42)
+
+    def train(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X).tolist()
